845839c9ecf682ab4a3df88574d9e16fa652cb51
yunuselektronika-ops <yunuselektronika@gmail.com>
2026-01-30 23:08:27 +0300

    Rebrand to Xpert Panel - initial modifications

---
+++ app/dashboard/src/constants/Project.ts
@@ -0,0 +1,3 @@
+export const REPO_URL = "https://github.com/yunuselektronika-ops/Xpert_Panel";
+export const ORGANIZATION_URL = "https://github.com/yunuselektronika-ops";
+export const DONATION_URL = "https://github.com/yunuselektronika-ops/Xpert_Panel";
